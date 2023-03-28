103f5e0c340e7de11a9f62dcfce353b926e757bd
Dakota Savage <djsavage0812@gmail.com>
2023-03-28 14:42:24 -0400

    Add Github Pages pipeline

--- src/App.svelte
+++ src/App.svelte
@@ -18,14 +18,6 @@
   <div class="card">
     <Counter />
   </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
@@ -40,8 +32,5 @@
   }
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
   }
 </style>
